502352aa4763e9d8092467fa84744bc8ea41a750
Júlio Cezar Koronati <jckoronati@hotmail.com>
2021-09-28 19:39:04 -0300

    first commit

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
+        const initialProps = await Document.getInitialProps(ctx);
+        
+        return {...initialProps};
+    }
+
+    render() {
+        return(
+            <Html>
+                <Head>
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}

46d1cf161ef37562098790d9a0e50ccf17545efd
Júlio Cezar Koronati <jckoronati@hotmail.com>
2021-09-28 20:15:20 -0300

    concluido ssr e ajustado tag img da pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetServerSideProps } from 'next';
 import Head from "next/head";
+import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from '../services/stripe';
 import styles from "../styles/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,12 +28,32 @@ export default function Home() {
           </h1>
           <p>
             Get acess to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
-        <img src="/images/avatar.svg" alt="Girl Coding" />
+        <Image height="520" width="334" src="/images/avatar.svg" alt="Girl Coding"/>
       </main>
     </>
   );
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JeT2CGNVC0IWEYjmyCALNBX', {
+    expand: ['product']
+  }); 
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    }).format(price.unit_amount / 100)
+  };
+
+  return {
+    props: {
+      product
+    }
+  }
 }

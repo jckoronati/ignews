44ccc19d344b046300a457e4f5a72a580cedf9bb
Júlio Cezar Koronati <jckoronati@hotmail.com>
2021-09-30 05:24:03 -0300

    adicionada autenticação via github

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -38,10 +38,8 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1JeT2CGNVC0IWEYjmyCALNBX', {
-    expand: ['product']
-  }); 
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JeT2CGNVC0IWEYjmyCALNBX'); 
 
   const product = {
     priceId: price.id,
@@ -54,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
